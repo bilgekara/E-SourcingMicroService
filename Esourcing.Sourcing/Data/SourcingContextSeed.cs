68ff55d9a1fc86fb4a8eb12d70f8ec7b46028748
bilgekara <karabilgenur@gmail.com>
2021-11-19 11:44:50 +0300

    context seed added

--- Esourcing.Sourcing/Data/SourcingContextSeed.cs
+++ Esourcing.Sourcing/Data/SourcingContextSeed.cs
@@ -9,37 +9,88 @@ namespace Esourcing.Sourcing.Data
 {
     public class SourcingContextSeed
     {
-        //public static void SeedData(IMongoCollection<Auction> auctionCollection)
-        //{
-        //    bool exist = auctionCollection.Find(p => true).Any();
-        //    if (!exist)
-        //    {
-        //        auctionCollection.InsertManyAsync(GetPreconfigureAuctions())
-        //    }
-        //}
+        public static void SeedData(IMongoCollection<Auction> auctionCollection)
+        {
+            bool exist = auctionCollection.Find(p => true).Any();
+            if (!exist)
+            {
+                auctionCollection.InsertManyAsync(GetPreconfigureAuctions());
+            }
+        }
 
-        //private static IEnumerable<Auction> GetPreconfigureAuctions()
-        //{
-        //    return new List<Auction>()
-        //    {
-        //        new Auction()
-        //        {
-        //            Name = "Auction 1",
-        //            Description ="Auction Desc 1",
-        //            CreatedAt = DateTime.Now,
-        //            StartedAt = DateTime.Now,
-        //            FinishedAt = DateTime.Now,
-        //            ProductId = "60093337093d7352d5467341",
-        //            IncludedSellers = new List<string>()
-        //            {
-        //                "seller1@test.com",
-        //                "seller2@test.com",
-        //                "seller3@test.com"
-        //            },
-        //            Quantity = 5,
-        //            Status =(int)Status.Active
-        //        }
-        //    }
-        //}
+        private static IEnumerable<Auction> GetPreconfigureAuctions()
+        {
+            return new List<Auction>()
+            {
+                new Auction()
+                {
+                    Name = "Auction 1",
+                    Description ="Auction Desc 1",
+                    CreatedAt = DateTime.Now,
+                    StartedAt = DateTime.Now,
+                    FinishedAt = DateTime.Now,
+                    ProductId = "60093337093d7352d5467341",
+                    IncludedSellers = new List<string>()
+                    {
+                        "seller1@test.com",
+                        "seller2@test.com",
+                        "seller3@test.com"
+                    },
+                    Quantity = 5,
+                    Status =(int)Status.Active
+                },
+                 new Auction()
+                {
+                    Name = "Auction 1",
+                    Description ="Auction Desc 1",
+                    CreatedAt = DateTime.Now,
+                    StartedAt = DateTime.Now,
+                    FinishedAt = DateTime.Now,
+                    ProductId = "60093337093d7352d5467341",
+                    IncludedSellers = new List<string>()
+                    {
+                        "seller1@test.com",
+                        "seller2@test.com",
+                        "seller3@test.com"
+                    },
+                    Quantity = 5,
+                    Status =(int)Status.Active
+                },
+                  new Auction()
+                {
+                    Name = "Auction 2",
+                    Description ="Auction Desc 2",
+                    CreatedAt = DateTime.Now,
+                    StartedAt = DateTime.Now,
+                    FinishedAt = DateTime.Now,
+                    ProductId = "60093337093d7352d5467341",
+                    IncludedSellers = new List<string>()
+                    {
+                        "seller1@test.com",
+                        "seller2@test.com",
+                        "seller3@test.com"
+                    },
+                    Quantity = 5,
+                    Status =(int)Status.Active
+                },
+                   new Auction()
+                {
+                    Name = "Auction 3",
+                    Description ="Auction Desc 3",
+                    CreatedAt = DateTime.Now,
+                    StartedAt = DateTime.Now,
+                    FinishedAt = DateTime.Now,
+                    ProductId = "60093337093d7352d5467341",
+                    IncludedSellers = new List<string>()
+                    {
+                        "seller1@test.com",
+                        "seller2@test.com",
+                        "seller3@test.com"
+                    },
+                    Quantity = 5,
+                    Status =(int)Status.Active
+                },
+            };
+        }
     }
 }
